ad2ee6285b98d17e67b764497d4cdd8929acb5b6
Marcos Ibañez <marcosaibanez87@gmail.com>
2024-05-13 15:17:14 -0300

    Primer Versión Módulo Admin

---
+++ www/Modules/Footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0.0
+    </div>
+    <strong>Copyright © 2024 <a href="#">Practicas Profesionalizantes 3</a>.</strong> Todos los derechos Reservados.
+</footer>
---
+++ www/Modules/Menu.php
@@ -0,0 +1,108 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->    
+    <a href="#" class="brand-link bg-white">
+    
+    <div class="container text-center">                
+                    <div class="col-xs-9">
+                        <img src="Resources/img/Agencia_1_F.png" class="col-xs-9" height="35%" width="35%" alt="">                
+                </div>
+            </div>    
+    </a>
+    
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+       
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            Productos
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../Templates.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Crear Producto</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../Templates.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Lista de Productos</p>
+                            </a>
+                        </li>                                           
+                    </ul>
+                </li>       
+                
+                <!--Customers-->
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            Proveedores
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../Templates.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Crear Proveedores</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../Templates.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Lista Proveedores</p>
+                            </a>
+                        </li>
+                                            
+                    </ul>
+                </li> 
+
+
+                <!--Vendors-->
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            Clientes
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../Clients.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Crear Clientes</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../Clients.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Lista Clientes</p>
+                            </a>
+                        </li>
+                                              
+                    </ul>
+                </li> 
+
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
