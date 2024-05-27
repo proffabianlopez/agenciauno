6852b829db8b8f473e7cd796535a386ea5b315e2
Mariano1234 <mariano22041993@gmail.com>
2024-05-27 11:56:20 -0300

    paginado

--- www/views/crud_suppliers.php
+++ www/views/crud_suppliers.php
@@ -257,6 +257,8 @@ $show=show_state("suppliers");
     <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="../dist/js/adminlte.min.js"></script>
+    <script src="../js/paginated.js"></script>
+    
     
 
 
