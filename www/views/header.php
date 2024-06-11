1bb88d2c75ab9eb9b69a03daa08c3351a3e7c9e4
Marcos Ibañez <marcosaibanez87@gmail.com>
2024-06-11 15:24:23 -0300

    Mejora estética Menu/Header

--- www/views/header.php
+++ www/views/header.php
@@ -6,25 +6,7 @@
               <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </ul>
       <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-          <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false" style="font-weight: bold;font-size: 20px">
-                  <i class="fas fa-light fa-user-circle fa-lg">     UserName</i> 
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
 
-                  <a class="dropdown-item"
-                      href="#">Cambiar
-                      Contraseña</a>
-
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item"
-                      href="../index.html">Salir</a>
-              </div>
-          </li>
-
-      </ul>
       
   </nav>
   <!-- /.navbar -->
