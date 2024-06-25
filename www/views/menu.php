89070638bc698e06d4a4e31336689de846429207
Marcos Ibañez <marcosaibanez87@gmail.com>
2024-06-24 22:29:47 -0300

    Mejora Menu

--- www/views/menu.php
+++ www/views/menu.php
@@ -1,4 +1,3 @@
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
@@ -7,7 +6,10 @@
         <div class="container text-center">
             <div class="col-xs-9">
                 <div class="card-header text-center">
-                    <a href="home.php" style="color: #007bff !important; text-decoration: none !important; background-color: transparent !important;"><h3 class="h3"><b>Agencia</b>UNO</h3></a>
+                    <a href="home.php"
+                        style="color: #007bff !important; text-decoration: none !important; background-color: transparent !important;">
+                        <h3 class="h3"><b>Agencia</b>UNO</h3>
+                    </a>
                 </div>
             </div>
         </div>
@@ -18,13 +20,13 @@
         <!-- Sidebar user (optional) -->
 
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-            <img src="../assets/img/app/person-circle.svg" class="img-circle elevation-2" alt="User Image">
+            <div class="image">
+                <img src="../assets/img/app/person-circle.svg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <h5 class="d-block" style="color:white">Usuario Principal</h5>
+            </div>
         </div>
-        <div class="info">
-            <h5 class="d-block" style="color:white">Usuario Principal</h5>
-        </div>
-    </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -57,34 +59,53 @@
                         </li>
                         <li class="nav-item">
 
-                            <a href="crud_products_new.php" class="nav-link">
-
-                                <i class="fa fa-microchip nav-icon"></i>
-                                <p>Productos</p>
+                            <a href="#" class="nav-link">
+                                <i class="fa fa-building nav-icon"></i>
+                                <p>Artículos <i class="fas fa-angle-left right"></i></p>
                             </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="crud_products_new.php" class="nav-link">
+                                        <i class="fa fa-microchip nav-icon"></i>
+                                        <p>Productos</p>
+                                    </a>
+
+                                    <a href="crud_brands_new.php" class="nav-link">
+
+                                        <i class="fa fa-indent nav-icon"></i>
+                                        <p>Marcas</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="crud_category.php" class="nav-link">
+                                        <i class="fa fa-indent nav-icon"></i>
+                                        <p>Categorias</p>
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
                         <li class="nav-item">
 
-                            <a href="crud_brands_new.php" class="nav-link">
+                            <a href="home.php" class="nav-link">
 
                                 <i class="fa fa-user nav-icon"></i>
-                                <p>Marcas</p>
+                                <p>Usuarios</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="crud_category.php" class="nav-link">
-                                <i class="fa fa-user nav-icon"></i>
-                                <p>Categorias</p>
-                            </a>
-                        </li>
+
                     </ul>
                 </li>
 
-            </ul>
-
         </nav>
+        <br><br>
+        <div class="info">
+            <a href="login.php" style="text-decoration: none !important">
+                <h5 class="d-block" style="color:white">&#128274; Cerrar Sesión</h5>
+            </a>
 
+        </div>
         <!-- /.sidebar-menu -->
-    </div>    
+    </div>
+
     <!-- /.sidebar -->
 </aside>
