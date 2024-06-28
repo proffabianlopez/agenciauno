b9948655ea43338a27f0e64186675383d383f616
Marcos Ibañez <marcosaibanez87@gmail.com>
2024-06-28 19:55:20 -0300

    Agregado de Sweet Alert en Clientes

--- www/controller/eliminar_cliente.php
+++ www/controller/eliminar_cliente.php
@@ -4,9 +4,9 @@ include_once "../models/functions.php";
 if (isset($_POST['edit-id_customer'])) {
     $id = $_POST['edit-id_customer'];
     
-    deletecliente($id);
-    echo '<script>alert("Borrado exitosamente");</script>';
-    echo '<script>window.location.href = "../views/crud_cliente.php";</script>';
+    deletecliente($id);    
+    
+    echo '<script>window.location.href = "../views/crud_cliente.php?mensaje=Usuario%20deshabilitado%20con%20éxito&tipo=success";</script>';
 } else {
     echo '<script>alert("No se recibió el ID del cliente.");</script>';
     echo '<script>window.location.href = "../views/crud_cliente.php";</script>';
